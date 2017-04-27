c3cfaf1055a30262585e8c985f9db1ef4efce10b
Loc Tran <tomtran43@gmail.com>
2017-04-27 09:38:37 +0700

    first commit

--- color/ViewController.swift
+++ color/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
 
 }
 
